335918cfdef497461cc89e3e92564bd4e013e4e5
Glenn De Jonghe <glenn@datacamp.com>
2016-01-28 21:44:25 +0100

    Use form to submit exercise data to datacamp light with a post request

--- src/datacamp-light.js
+++ src/datacamp-light.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var MIN_HEIGHT = 200;
+var IFRAME_NAME = "datacamp-light-iframe";
 var DATACAMP_LIGHT_URL = "https://light.datacamp.com/";
 
 /**
@@ -74,23 +75,119 @@ function createURLData(data) {
 	var result = [];
 	for (var entry in data) {
 		if (data.hasOwnProperty(entry))
-			result.push(encodeURIComponent(entry) + "=" + encodeURIComponent(stripIndent(data[entry])));
+			result.push(encodeURIComponent(entry) + "=" + encodeURIComponent(data[entry]));
 	}
 	return result.join("&");
+}
+
+function createIFrame(exercise_DOM, exercise_data) {
+	var iframe = document.createElement("iframe");
+	iframe.setAttribute("name", IFRAME_NAME);
+	var style_attribute = "border: 1px solid #d5eaef;";
+
+	// Calculate height of iframe
+	var doHeightCalculation = true;
+	var height;
+	if ("height" in exercise_DOM.dataset) {
+		var user_height = Math.round(exercise_DOM.dataset["height"]);
+		if (!isNaN(user_height)) {
+			if (user_height > MIN_HEIGHT) {
+				height = user_height;
+				doHeightCalculation = false;
+			} else {
+				console.log("The height attribute should be larger than " + MIN_HEIGHT + ".");
+			}
+		} else {
+			console.log("Invalid height attribute.");
+		}
+	}
+
+	if (doHeightCalculation) {
+		// Get min-height
+		var min_height = MIN_HEIGHT;
+		if ("minHeight" in exercise_DOM.dataset) {
+			var user_min_height = Math.round(exercise_DOM.dataset["minHeight"]);
+			if (!isNaN(user_min_height)) {
+				if (user_min_height > MIN_HEIGHT) {
+					min_height = user_min_height;
+				} else {
+					console.log("The min-height attribute should be larger than " + MIN_HEIGHT + ".");
+				}
+			} else {
+				console.log("Invalid min-height attribute.");
+			}
+		}
+
+		// Get height based on sample-code
+		var sample_lines_length = exercise_data["sample-code"].split(/\r?\n/).length;
+		height = Math.max(
+			47 + (sample_lines_length-2) * 18,
+			min_height
+		);
+
+		if ("maxHeight" in exercise_DOM.dataset) {
+			var user_max_height = Math.round(exercise_DOM.dataset["maxHeight"]);
+			if (!isNaN(user_max_height)) {
+				if (user_max_height > MIN_HEIGHT) {
+					height = Math.min(height, user_max_height);
+				} else {
+					console.log("The max-height attribute should be larger than " + MIN_HEIGHT + ".");
+				}
+			} else {
+				console.log("Invalid max-height attribute.");
+			}
+		}
+	}
+
+	iframe.style.height = height + "px";
+	style_attribute += "height:" + height + "px;"
+
+	// Set width and min-width
+	iframe.style.width = "100%";
+	iframe.style.minWidth = "600px";
+	style_attribute += "width:100%;min-width:600px;"
+
+	// Set style attribute of iframe
+	iframe.setAttribute("style", style_attribute);
+	return iframe;
+}
+
+function createDataForm(exercise_data) {
+	var url = DATACAMP_LIGHT_URL + (window.location.hostname ? window.location.hostname + "/?" : "?") +
+	createURLData({
+
+	});
+    var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", DATACAMP_LIGHT_URL);
+    form.setAttribute("target", IFRAME_NAME);
+
+    for(var key in exercise_data) {
+        if(exercise_data.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", stripIndent(exercise_data[key]));
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    return form;
 }
 
 function replaceDataCampExercises() {
 	var exercises = document.querySelectorAll("[data-datacamp-exercise]");
 	for (var i = 0; i < exercises.length; i++) {
-		var exercise = exercises[i];
+		var exercise_DOM = exercises[i];
 
-		if (!("lang" in exercise.dataset)) {
+		if (!("lang" in exercise_DOM.dataset)) {
 			console.log("DataCamp-Light: Missing the data-lang attribute.");
 			return;
 		}
 
-		var result_object = {
-			"language": exercise.dataset["lang"],
+		var exercise_data = {
+			"language": exercise_DOM.dataset["lang"],
 			"pre-exercise-code": "",
 			"sample-code": "",
 			"solution": "",
@@ -98,87 +195,22 @@ function replaceDataCampExercises() {
 			"hint": "",
 		}
 
-		processParagraphTags(result_object, exercise.getElementsByTagName('p'));
-		processCodeTags(result_object, exercise.getElementsByTagName('code'));
+		processParagraphTags(exercise_data, exercise_DOM.getElementsByTagName('p'));
+		processCodeTags(exercise_data, exercise_DOM.getElementsByTagName('code'));
 
 		// Actually replace
-		while (exercise.lastChild) {
-		    exercise.removeChild(exercise.lastChild);
+		while (exercise_DOM.lastChild) {
+		    exercise_DOM.removeChild(exercise_DOM.lastChild);
 		}
-
-		// Add iframe
-		var url = DATACAMP_LIGHT_URL + (window.location.hostname ? window.location.hostname + "/?" : "?") + createURLData(result_object);
-		var iframe = document.createElement("iframe");
-		iframe.setAttribute("src", url);
-		var style_attribute = "border: 1px solid #d5eaef;";
-
-		// Calculate height of iframe
-		var doHeightCalculation = true;
-		var height;
-		if ("height" in exercise.dataset) {
-			var user_height = Math.round(exercise.dataset["height"]);
-			if (!isNaN(user_height)) {
-				if (user_height > MIN_HEIGHT) {
-					height = user_height;
-					doHeightCalculation = false;
-				} else {
-					console.log("The height attribute should be larger than " + MIN_HEIGHT + ".");
-				}
-			} else {
-				console.log("Invalid height attribute.");
-			}
-		}
-
-		if (doHeightCalculation) {
-			// Get min-height
-			var min_height = MIN_HEIGHT;
-			if ("minHeight" in exercise.dataset) {
-				var user_min_height = Math.round(exercise.dataset["minHeight"]);
-				if (!isNaN(user_min_height)) {
-					if (user_min_height > MIN_HEIGHT) {
-						min_height = user_min_height;
-					} else {
-						console.log("The min-height attribute should be larger than " + MIN_HEIGHT + ".");
-					}
-				} else {
-					console.log("Invalid min-height attribute.");
-				}
-			}
-
-			// Get height based on sample-code
-			var sample_lines_length = result_object["sample-code"].split(/\r?\n/).length;
-			height = Math.max(
-				47 + (sample_lines_length-2) * 18,
-				min_height
-			);
-
-			if ("maxHeight" in exercise.dataset) {
-				var user_max_height = Math.round(exercise.dataset["maxHeight"]);
-				if (!isNaN(user_max_height)) {
-					if (user_max_height > MIN_HEIGHT) {
-						height = Math.min(height, user_max_height);
-					} else {
-						console.log("The max-height attribute should be larger than " + MIN_HEIGHT + ".");
-					}
-				} else {
-					console.log("Invalid max-height attribute.");
-				}
-			}
-		}
-
-		iframe.style.height = height + "px";
-		style_attribute += "height:" + height + "px;"
-
-		// Set width and min-width
-		iframe.style.width = "100%";
-		iframe.style.minWidth = "600px";
-		style_attribute += "width:100%;min-width:600px;"
-
-		// Set style attribute of iframe
-		iframe.setAttribute("style", style_attribute);
 
 		// Append iframe
-		exercise.appendChild(iframe);
+		exercise_DOM.appendChild(createIFrame(exercise_DOM, exercise_data));
+
+		// Create form to send exercise data
+		var form = createDataForm(exercise_data);
+		exercise_DOM.appendChild(form);
+		form.submit();
+		// TODO REMOVE FORM AGAIN?
 	}
 }
 

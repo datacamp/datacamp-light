063556534aeb138d97589174f1dc7f2db9b640aa
karar-vir <52925987+karar-vir@users.noreply.github.com>
2019-09-15 22:44:07 +0530

    2nd_github_Bitwise_operators.py

---
+++ 2nd_github_Bitwise_operators.py
@@ -0,0 +1,11 @@
+# Bitwise Operators in Python 
+# AND is denoted by '&' and OR is denoted by '|' 
+# XOR is denoted by '^' , Bitwise Compliment '~'
+
+a,b=15,10   # a=10 , b=5 
+
+print(a & b)   # 10 = 00001010 & 5=00000101  output-10
+
+print(a | b)  # 10 = 00001010 | 5=00000101  output-15
+
+print( a ^ b) # 10 = 00001010 ^ 5=00000101 output-5

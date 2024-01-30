86a545cd19fcf5b4933d5239c75accff61885977
Lariveg <larisa.popescu@datacamp.com>
2024-01-30 10:43:52 +0100

    feat: add home button link parameter

--- src/boot.tsx
+++ src/boot.tsx
@@ -30,6 +30,8 @@ export default (element: HTMLDivElement, hub: Hub) => {
 
   const utmSource = element.getAttribute("data-utm-source") || undefined;
   const utmCampaign = element.getAttribute("data-utm-campaign") || undefined;
+  const homeButtonLink =
+    element.getAttribute("data-home-button-link") || undefined;
 
   if (element.getAttribute("data-encoded")) {
     const exercise = JSON.parse(atob(decodeURIComponent(element.textContent)));
@@ -113,6 +115,7 @@ export default (element: HTMLDivElement, hub: Hub) => {
             language={settings.language}
             utmSource={utmSource}
             utmCampaign={utmCampaign}
+            homeButtonLink={homeButtonLink}
           />
         </Provider>
       </AppContainer>
